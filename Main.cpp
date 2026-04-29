a64ad9d27f22aea4ca5e86c785ba5aee4dce877b
ST <107823200+st037@users.noreply.github.com>
2026-04-29 11:26:30 +0900

    I changed

--- Main.cpp
+++ Main.cpp
@@ -2,24 +2,94 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "shaderClass.h"
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
+#include "Texture.h"
+#include "Camera.h"
 
-GLfloat vertices[] =
-{// COORDINATES           / COLORS                //
-	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,     0.0f, 0.0f,
-	-0.5f,	0.5f, 0.0f,     0.0f, 1.0f, 0.0f,     0.0f, 1.0f,
-	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
-	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,     1.0f, 0.0f,
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+/*GLfloat vertices[] =
+{   // COORDINATES          / COLORS                 //TexCoord      // NORMALS
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f,     0.0f, -1.0f, 0.0f, //Bottom side
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 5.0f,     0.0f, -1.0f, 0.0f, //Bottom side
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 5.0f,     0.0f, -1.0f, 0.0f, //Bottom side
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 0.0f,     0.0f, -1.0f, 0.0f, //Bottom side
+
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f,    -0.8f,  0.5f, 0.0f, //Left side
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 0.0f,    -0.8f,  0.5f, 0.0f, //Left side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.80f, 0.76f,     2.5f, 5.0f,    -0.8f,  0.5f, 0.0f, //Left side
+
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f,     0.0f,  0.5f, -0.8f, //Non-facing side
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 0.0f,     0.0f,  0.5f, -0.8f, //Non-facing side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.80f, 0.76f,     2.5f, 5.0f,     0.0f,  0.5f, -0.8f, //Non-facing side
+
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 0.0f,    -0.8f,  0.5f, 0.0f, //Right side
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f,    -0.8f,  0.5f, 0.0f, //Right side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.80f, 0.76f,     2.5f, 5.0f,	-0.8f,  0.5f, 0.0f, //Right side
+
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     0.0f, 0.0f,     0.0f,  0.5f, -0.8f, //Facing side
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,     5.0f, 0.0f,     0.0f,  0.5f, -0.8f, //Facing side
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.80f, 0.76f,     2.5f, 5.0f,     0.0f,  0.5f, -0.8f, //Facing side
 };
 
 GLuint indices[] =
 {
-	0, 2, 1,
-	0, 3, 2,
+	0, 1, 2,
+	0, 2, 3,
+	4, 6, 5,
+	7, 9, 8,
+	10, 12, 11,
+	13, 15, 14,
+};*/
+
+GLfloat vertices[] = 
+{
+		-1.0f, 0.0f,  1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f,   0.0f, 1.0f, 0.0f,
+		-1.0f, 0.0f, -1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 1.0f,   0.0f, 1.0f, 0.0f,
+		 1.0f, 0.0f, -1.0f,   0.0f, 0.0f, 0.0f,   1.0f, 1.0f,   0.0f, 1.0f, 0.0f,
+		 1.0f, 0.0f,  1.0f,   0.0f, 0.0f, 0.0f,   1.0f, 0.0f,   0.0f, 1.0f, 0.0f,
+};
+
+GLuint indices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+};
+
+GLfloat lightVertices[] =
+{
+	-0.1f, -0.1f,  0.1f,
+	-0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f,  0.1f,
+	-0.1f,  0.1f,  0.1f,
+	-0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f,  0.1f,
+};
+
+GLuint lightIndices[] =
+{
+	0,1,2,
+	0,2,3,
+	0,4,7,
+	0,7,3,
+	3,7,6,
+	3,6,2,
+	2,6,5,
+	2,5,1,
+	1,5,4,
+	1,4,0,
+	4,5,6,
+	4,6,7,
 };
 
 int main()
@@ -30,18 +100,18 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "opengl_game_engine", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "opengl_game_engine", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		return - 1;
+		return -1;
 	}
 	glfwMakeContextCurrent(window);
 
 	gladLoadGL();
 
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	Shader shaderProgram("default.vert", "default.frag");
 
@@ -51,49 +121,84 @@ int main()
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float)));
+
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+	Shader lightShader("light.vert", "light.frag");
 
-	int widthImage, heightImage, numColCh;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char* bytes = stbi_load("pop_cat.png", &widthImage, &heightImage, &numColCh, 0);
+	VAO lightVAO;
+	lightVAO.Bind();
 
-	GLuint texture;
-	glGenTextures(1, &texture);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	VBO lightVBO(lightVertices, sizeof(lightVertices));
+	EBO lightEBO(lightIndices, sizeof(lightIndices));
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	lightVAO.Unbind();
+	lightVBO.Unbind();
+	lightEBO.Unbind();
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImage, heightImage, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
-	stbi_image_free(bytes);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
 
-	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 pyramidModel = glm::mat4(1.0f);
+	pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+	lightShader.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+	glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	shaderProgram.Activate();
-	glUniform1i(tex0Uni, 0);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
+	//GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	Texture planksTex("planks.png", GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE);
+	planksTex.texUnit(shaderProgram, "tex0", 0);
+	Texture planksSpec("planksSpec.png", GL_TEXTURE_2D, 1, GL_RED, GL_UNSIGNED_BYTE);
+	planksSpec.texUnit(shaderProgram, "tex1", 1);
+
+	//float rotation = 0.0f;
+	//double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
+
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		camera.Inputs(window);
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
 		shaderProgram.Activate();
-		glUniform1f(uniID, 0.5f);
-		glBindTexture(GL_TEXTURE_2D, texture);
+		glUniform3f(glGetUniformLocation(shaderProgram.ID,"camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+		camera.Matrix(shaderProgram, "camMatrix");
+
+		planksTex.Bind();
+		planksSpec.Bind();
+
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
+		lightShader.Activate();
+		camera.Matrix(lightShader, "camMatrix");
+		lightVAO.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -101,10 +206,10 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
-	glDeleteTextures(1, &texture);
+	planksTex.Delete();
 	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
-}
+};
--- default.vert
+++ default.vert
@@ -1,16 +1,29 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTex;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec3 aColor;
+layout (location = 3) in vec2 aTex;
 
+out vec3 crntPos;
+out vec3 Normal;
 out vec3 color;
 out vec2 texCoord;
 
-uniform float scale;
+/*uniform float scale;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;*/
+
+uniform mat4 camMatrix;
+uniform mat4 model;
 
 void main()
 {
-	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	crntPos = vec3(model * vec4(aPos, 1.0f));
+	Normal = aNormal;
 	color = aColor;
 	texCoord = aTex;
+
+	gl_Position = camMatrix * vec4(crntPos, 1.0);
 }
